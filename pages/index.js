f358970dc3a5645bb1d1ba01a2ad158a7ca304a5
Jimmy Lu <lujianming000@gmail.com>
2021-08-06 15:08:55 -0700

    LG-FE

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+const Index  = () => {
+    return (
+        <>
+        <h1 className="jumbotron text-center bg-primary square">Course Gallery</h1>
+        <p>a lot of courses...</p>
+        </>
+    );
+};
+
+export default Index;
