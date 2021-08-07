3fb342767361099de96fbbbfa38970ed992b2610
Jimmy Lu <lujianming000@gmail.com>
2021-08-06 17:02:50 -0700

    test 5

--- components/routes/UserRoute.js
+++ components/routes/UserRoute.js
@@ -16,7 +16,7 @@ const UserRoute = ({ children }) => {
 
   const fetchUser = async () => {
     try {
-      const { data } = await axios.get("/api/current-user");
+      const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API}/api/current-user`);
       //   console.log(data);
       if (data.ok) setOk(true);
     } catch (err) {
